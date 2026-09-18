1c3bccfba3525a6ca67fa7f3688460819b7a0d8c
resisigu <148083782+resisigu@users.noreply.github.com>
2026-09-18 23:02:48 +0900

    build: configure Paper 26.2 and Java 25

---
+++ build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+    java
+}
+
+group = "org.easyitems"
+version = "0.1.0"
+
+repositories {
+    mavenCentral()
+    maven {
+        name = "papermc"
+        url = uri("https://repo.papermc.io/repository/maven-public/")
+    }
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:26.2.build.+")
+}
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(25))
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    options.encoding = "UTF-8"
+    options.release.set(25)
+}
+
+tasks.processResources {
+    filesMatching("plugin.yml") {
+        expand("version" to project.version)
+    }
+}
+
+tasks.jar {
+    archiveBaseName.set("EasyItems")
+}
